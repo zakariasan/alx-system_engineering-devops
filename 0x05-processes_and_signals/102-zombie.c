4219de5abd07a5d886b98110dfbf839abf582e56
zakariasan <elitziko@gmail.com>
2023-11-24 13:51:41 +0100

    zobies and infinity beyond

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,22 +3,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 /**
- * infinite_while- For every zombie process created, it displays 
- * Return: Zero 
+ * infinite_while- For every zombie process created, it displays
+ * Return: Zero
  */
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
  * main- a C program that creates 5 zombie processes.
- * Return: return 0 
+ * Return: return 0
  */
 int main(void)
 {
@@ -32,6 +32,7 @@ int main(void)
 			exit(0);
 		else
 			printf("Zombie process created, PID: %d\n", pid);
+		i++;
 	}
 	infinite_while();
 	return (0);
