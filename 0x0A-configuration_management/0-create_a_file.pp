57ab6b37e6515748126ca3774560b1552e1baca3
zakariasan <elitziko@gmail.com>
2024-01-19 23:33:04 +0100

    puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 file { '/tmp/school':
-  ensure  => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
