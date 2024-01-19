1bbcab2f96e71e34afeb93f0f7fb1fb9a09d1273
zakariasan <elitziko@gmail.com>
2024-01-20 00:22:27 +0100

    install pack

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, install flask from pip3.
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
