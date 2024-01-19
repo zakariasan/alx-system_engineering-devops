6166daebe45e88db03b31a7992dd0e8ee9f92baa
zakariasan <elitziko@gmail.com>
2024-01-20 00:00:40 +0100

    commenting and executing commands

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,13 +1,5 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
 
-exec { 'killmenow':
-  command     => '/usr/bin/pkill -f "killmenow"',
-  refreshonly => true,
-  subscribe   => File['/path/to/killmenow_script'],
-}
-
-file { '/path/to/killmenow_script':
-  ensure  => present,
-  mode    => '0755',
-  content => "#!/bin/bash\nwhile true; do sleep 2; done\n",
+exec { 'pkill':
+  command  => 'pkill killmenow',
 }
