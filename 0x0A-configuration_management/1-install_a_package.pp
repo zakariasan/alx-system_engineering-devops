d7ebcac405448da2204096de93e3a83a1e30b8eb
zakariasan <elitziko@gmail.com>
2024-01-20 00:43:30 +0100

    install and packing flask2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #Using Puppet, install flask from pip3.
 
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
