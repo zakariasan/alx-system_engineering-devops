97859fca4420d2090051b85114e77c7aa18703bb
zakariasan <elitziko@gmail.com>
2024-01-19 23:19:35 +0100

    config puppet

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,11 @@
+exec { 'killmenow':
+  command     => '/usr/bin/pkill -f "killmenow"',
+  refreshonly => true,
+  subscribe   => File['/path/to/killmenow_script'],
+}
+
+file { '/path/to/killmenow_script':
+  ensure => present,
+  mode   => '0755',
+  content => "#!/bin/bash\nwhile true; do sleep 2; done\n",
+}
