4ada341abd12130500a5a9491e387e41668aae4c
zakariasan <elitziko@gmail.com>
2024-01-22 18:55:37 +0100

    pup conf pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,12 +6,12 @@ file_line { 'Turn off passwd auth':
   ensure  => 'present',
   path    => '/etc/ssh/sshd_config',
   line    => '    PasswordAuthentication no',
-  replace => 'true'
+  replace => 'true',
 }
 
 file_line { 'Declare identity file':
   ensure  => 'present',
   path    => '/etc/ssh/sshd_config',
   line    => '     IdentityFile ~/.ssh/school',
-  replace => 'true'
+  replace => 'true',
 }
