f789f79520ae6583b8790b9a55874596a8259a15
zakariasan <elitziko@gmail.com>
2024-01-23 02:07:07 +0100

    match puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,11 @@
 #  Bash script that uses ssh to connect to your server using the private key 
 # 100-puppet_ssh_config.pp
-include stdlib
 
-file_line { 'Turn off passwd auth':
-  ensure  => 'present',
-  path    => '/etc/ssh/sshd_config',
-  line    => '    PasswordAuthentication no',
-  replace => 'true',
-}
-
-file_line { 'Declare identity file':
-  ensure  => 'present',
-  path    => '/etc/ssh/sshd_config',
-  line    => '     IdentityFile ~/.ssh/school',
-  replace => 'true',
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content =>"
+	host *
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+ "
 }
