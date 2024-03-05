a10527d270fb4111b51d4b83310885d33e0dd958
zakariasan <elitziko@gmail.com>
2024-03-06 00:02:47 +0100

    fix : comments3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ Queries the reddit api """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My-User-Agent'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirections=False)
     data = response.json()
     if 'data' in data and 'subscribers' in data['data']:
         return data['data']['subscribers']
