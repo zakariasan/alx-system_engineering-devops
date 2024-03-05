c6cc138ef9c386b9dd19659c854cd41fcaf2ceef
zakariasan <elitziko@gmail.com>
2024-03-05 22:29:35 +0100

    api advanced01

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" playing with queering """
+import requests
+from collections import Counter
+
+
+def count_words(subreddit, word_list, after=None, word_counts=None):
+    """ wrod counts like aois let's play """
+    if word_counts is None:
+        word_counts = Counter()
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    headers = {'User-Agent': 'My-User-Agent'}
+    params = {'after': after} if after else {}
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                title = post['data']['title'].lower()
+                word_counts.update(filter(lambda w: w.lower() in
+                                          title.split(), word_list))
+
+            if data['data']['after'] is not None:
+                return count_words(subreddit,
+                                   word_list,
+                                   data['data']['after'],
+                                   word_counts)
+            else:
+                sorted_word_counts = sorted(word_counts.items(),
+                                            key=lambda x: (-x[1], x[0]))
+                for word, count in sorted_word_counts:
+                    print(word, count)
+        else:
+            return
+    except Exception as e:
+        print("Error:", e)
+        return
