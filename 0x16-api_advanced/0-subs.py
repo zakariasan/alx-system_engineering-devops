5b1426fc16282b3a05715fecddf486287fcc4b91
zakariasan <elitziko@gmail.com>
2024-03-06 00:06:35 +0100

    fix : comment1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Req reddit api """
-import requests
 
 
 def number_of_subscribers(subreddit):
     """ Queries the reddit api """
+    import requests
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My-User-Agent'}
     response = requests.get(url, headers=headers, allow_redirections=False)
