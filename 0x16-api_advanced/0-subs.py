75ef01226943f40a57f9071c16c373652ac5fbde
zakariasan <elitziko@gmail.com>
2024-03-06 18:13:26 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Module for Req reddit api function that queries the Reddit API and returns
-the number of subscribers"""
+"""
+Queries the Reddit API for the number of subscribers to a given subreddit.
+Returns the number of subscribers if the subreddit exists, otherwise ret 0.
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the reddit api for a given subreddit. If an invalid subreddit
-    is given, the function should return 0.
+    Queries the Reddit API for the number of subscribers to a given subreddit.
+    Returns the number of subscribers if the subreddit exists, otherwise ret 0.
     """
-    import requests
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'python:myredditapp:v1.0.0 (by /u/zakaria_H/)'}
-    response = requests.get(url, headers=headers, allow_redirections=False)
-    data = response.json()
-
-    if response.status_code >= 300:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException as e:
+        print("Error:", e)
         return 0
-    else:
-        return data['data']['subscribers']
